bcb4d90028c260ca648a3c0d83425276e5015d44
awemulya <awemulya@gmail.com>
2019-03-23 05:47:20 +0545

    basic exercise on dictionary

---
+++ dict/fs_question.py
@@ -0,0 +1,3 @@
+question_dict = {"default_language": "default", "id_string": "afxkvFvEfVDeQ8qP6ujuJ6", "children": [{"na    me": "start", "type": "start"}, {"name": "end", "type": "end"}, {"name": "name", "bind": {"required"    : "false"}, "label": "name", "type": "text"}, {"name": "__version__", "bind": {"calculate": "'vX2TDy    LMzzFWuL4NdC5Ssd'"}, "type": "calculate"}, {"control": {"bodyless": True}, "children": [{"name": "in    stanceID", "bind": {"readonly": "true()", "calculate": "concat('uuid:', uuid())"}, "type": "calculat    e"}], "name": "meta", "type": "group"}], "version": "vX2TDyLMzzFWuL4NdC5Ssd", "type": "survey", "nam    e": "afxkvFvEfVDeQ8qP6ujuJ6", "sms_keyword": "afxkvFvEfVDeQ8qP6ujuJ6", "title": "john"}
+
+print(question_dict)
