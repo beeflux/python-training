25b70cc1a17e87f888bf7c68a47185eaa7bc26dc
ghimireadarsh <adarshghimire5@gmail.com>
2019-03-23 10:41:08 +0545

    prime modified

--- prime.py
+++ prime.py
@@ -3,6 +3,8 @@ def primeCal(*args):
     for i in args:
         j = 2;
         flag = 1
+        if i == 0 or i == 1:
+            flag = 0
         while j<i:
             if i%j==0:
                 flag = 0
@@ -12,6 +14,6 @@ def primeCal(*args):
             prime_numbers.append(i)
 
     return prime_numbers
-x = primeCal(1,2,3,4,5,6)
+x = primeCal(0,1,2,3,4,5,6)
 print('Primenumber are :')
 print(x)
