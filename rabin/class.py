f6f523fbccc65077c524386bb4553c804d91e3b7
rabin <xhrrabin@gmail.com>
2019-03-30 10:14:12 +0545

    rabin folder is added and class.py is created

---
+++ rabin/class.py
@@ -0,0 +1,3 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<groupuinames:template-group-list xmlns:groupuinames="http://openoffice.org/2006/groupuinames"><groupuinames:template-group groupuinames:name="student" groupuinames:default-ui-name="student"/>
+</groupuinames:template-group-list>
