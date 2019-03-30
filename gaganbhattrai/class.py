7dfed9e2e6d6a9bc4351be2afd656d3ade8202fd
gagan bhattarai <gaganbhattarai1993@gmail.com>
2019-03-30 10:40:01 +0545

    college class is modified

--- gaganbhattrai/class.py
+++ gaganbhattrai/class.py
@@ -1,12 +1,23 @@
 class College:
-	def__init__(self,name,address):
+	def __init__(self, name, address, course):
 
 		self.name = name
 		self.address = address
 		self.course = []
 	
-	def add_course(self,course):
+	def add_course(self, course):
 		self.course.append(course)
+
+c = College("college of applied bussiness","chhabahil",[])
+d = College("st xavier college","chhabahil",[])
+
+c.add_course("csit")
+d.add_course("BBA")
+
+print(c.course)
+print(d.course)
+
+
 
 
 		
