44d3720206477a277ff3b323b1fa875a75665043
gagan bhattarai <gaganbhattarai1993@gmail.com>
2019-03-30 10:18:52 +0545

    files added

---
+++ gaganbhattrai/class.py
@@ -0,0 +1,12 @@
+class College:
+	def__init__(self,name,address):
+
+		self.name = name
+		self.address = address
+		self.course = []
+	
+	def add_course(self,course):
+		self.course.append(course)
+
+
+		
