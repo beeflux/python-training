21f4aaf36b85ace93fc6604a13b774a14240f078
ghimireadarsh <adarshghimire5@gmail.com>
2019-04-05 23:46:30 +0545

    package file upto prblem 7 made

--- project_euler/__init__.py
+++ project_euler/__init__.py
@@ -3,3 +3,5 @@ from problem2 import fibonacci, fibonacci_upto
 from problem3 import max_prime_factor
 from problem4 import largest_palindrome
 from problem5 import smallest_divisible
+from problem6 import difference_calculator
+from problem7 import index_prime
