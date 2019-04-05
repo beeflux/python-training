c32d07ec055e049301370c929d9c3e43d42b289a
ghimireadarsh <adarshghimire5@gmail.com>
2019-04-05 22:53:31 +0545

    problem 4 and 5 package initialized

--- project_euler/__init__.py
+++ project_euler/__init__.py
@@ -1,3 +1,5 @@
 from problem1 import sum_multiples_3_5
 from problem2 import fibonacci, fibonacci_upto
 from problem3 import max_prime_factor
+from problem4 import largest_palindrome
+from problem5 import smallest_divisible
