cb4bc99eacc87ce97bd71364f1368471e330ffa7
ghimireadarsh <adarshghimire5@gmail.com>
2019-04-06 10:18:05 +0545

    bike documentation

--- classes/bike.py
+++ classes/bike.py
@@ -1,3 +1,26 @@
+"""
+    Bike is the derived class of Parent class Vehicle
+    Bike Object inherits all the properties of the Vehicle class
+
+    Bike class cosists of :
+
+    *   Constructor __init__ takes 3 arguments:
+            -   kind
+            -   engine
+            -   speed
+
+    *   setage()
+            - sets the age of the bike object
+
+    *   servicing()
+            -   Checks if age of the object
+            -   Prints the suggestion regarding servicing
+
+    *    print_info()
+            -   prints the property of the object
+"""
+
+
 from vehicle import Vehicle
 
 class Bike(Vehicle):
@@ -7,7 +30,7 @@ class Bike(Vehicle):
         self.topspeed = speed
         self.age = 0
 
-    def setAge(self, age):
+    def setage(self, age):
         self.age = age
 
     def servicing(self):
@@ -19,14 +42,17 @@ class Bike(Vehicle):
 
         else:
             print('Your {} is in emergency'.format(self.kind))
+
     def print_info(self):
         print("Your bike is {}, with engine power {} and topspeed = {} "
         .format(self.kind,self.engine,self.topspeed))
         self.servicing()
 
 if __name__ == '__main__':
-    # v = Vehicle("Car", 200, "Ba 1 ja 2015", color="red")
-    # print(v.plate, "is the plate no")
+    v = Vehicle("Car", 200, "Ba 1 ja 2015", color="red")
+    print("The plate no is {}".format(v.plate))
     b = Bike('racing','400HP','300km/hr')
+
+    print(b.plate)
     b.setAge(1)
     b.print_info()
