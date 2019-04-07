617767c690e636aaf169d5da309022793bb98a13
rajesh <pudasainirajesh504@gmail.com>
2019-04-07 20:52:05 +0545

    added some documentation to pedal_bike.py

--- classes/pedal_bike.py
+++ classes/pedal_bike.py
@@ -1,35 +1,50 @@
-from bike import Bike
+"""
+	Here is simple example of Inheritance, where we import Bike
+	from file bike and used the property of class Bike into Pedalcycle
 
-class PedalBike(Bike):
+	Here is the class PedalBike and is the child of class Bike
+"""
+
+from bike import Bike
+from vehicle import Vehicle
+
+class PedalBike(Bike, Vehicle):
+	#initializing the default value
 	def __init__(self, gear, backseat, height, color):
 		self.gear = gear
 		self.backseat = backseat
 		self.height = height
 		self.color = color
 
+	#it asks user to import all property he/she wants in his cycle
 	def get_cycle_property(self):
-		self.gear = int(input("No. of gear you want: "))
-		self.backseat = input("Do you need backseat: ")
+		self.gear = input("Need Gear in cycle (y/n): ")
+		self.backseat = input("Do you need backseat (y/n): ")
 		self.height = int(input("Size of cycle height: "))
 		self.color = input("Cycle Color: ")
 		self.price()
-		Bike.setAge(5)
+		Bike.setAge(self, 5)
 
+	#according to the user's demand it shows up the price
 	def price(self):
-		if (self.gear>0) and (self.backseat == "YES" or "yes"):
-			print(f"The Cycle with Gear {self.gear} price ranges from 10k to 100k")
+		if (self.gear.lower()=='y'):
+			if (self.backseat.lower() == "y"):
+				print("\t\t\t\tPrice of bicycle: 10,000\n\n")
+			else:
+				print("\t\t\t\tPrice of bicycle: 8,000\n\n")
 		else:
-			print("The Cycle costs price ranges from 10k to 50k")
+			print("\t\t\t\t\tPrice of bicycle: 5,000\n\n")
 
+	#condition to check whether the bicycle has disc brake or not
 	def brake(self):
-		print("Front and Rear both has DiscBrake")
-
-	def check_condition(self):
-		print("Is in Condition")
+		self.brake = input("Do you want Disc Brake in both front and rear(y/n): ")
+		if (self.brake.lower() == 'y'):
+			print("You need to pay additional 2000 for it: ")
+		else:
+			print("Ok:):)")
 
 if __name__ == '__main__':
 	cycle1 = PedalBike(0, "YES", 0, "red")
 	cycle1.get_cycle_property()
 	# cycle1.price()
 	cycle1.brake()
-	cycle1.check_condition()
